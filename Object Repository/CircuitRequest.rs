6b21887de38a7cfe2c10f5746eb205517337c956
lkkushan101 <kam@codehousegroup.com>
2018-06-10 11:27:31 +0530

    v1

---
+++ Object Repository/CircuitRequest.rs
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebServiceRequestEntity>
+   <description></description>
+   <name>CircuitRequest</name>
+   <tag></tag>
+   <elementGuidId>83d912b5-d862-46c0-9b60-bdd310f54c20</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <httpBody></httpBody>
+   <restRequestMethod>GET</restRequestMethod>
+   <restUrl>http://ergast.com/api/f1/2017/circuits.json</restUrl>
+   <serviceType>RESTful</serviceType>
+   <soapBody></soapBody>
+   <soapHeader></soapHeader>
+   <soapRequestMethod></soapRequestMethod>
+   <soapServiceFunction></soapServiceFunction>
+   <wsdlAddress></wsdlAddress>
+</WebServiceRequestEntity>
